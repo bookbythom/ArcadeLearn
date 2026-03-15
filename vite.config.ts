3c38d9b66eda9c586c72b6954090884af7dd53c9
Alexej Thomas Knoll <alexejthomasknoll@MacBook-Pro.local>
2026-03-15 14:42:00 +0100

    stable

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
@@ -7,9 +6,8 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      'supabase': path.resolve(__dirname, './supabase'),
-      'utils': path.resolve(__dirname, './utils'),
+      '@': '/src',
+      'supabase': '/supabase',
     },
   },
   optimizeDeps: {
